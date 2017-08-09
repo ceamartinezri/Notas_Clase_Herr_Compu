4ea9ee1e8ae425fa7390370faa90423e922d57e3
ceamartinezri <ceamartinezri@unal.edu.co>
2017-08-09 10:56:55 -0500

    clase 09-08

---
+++ 09-08-clase/holamundo.cpp
@@ -0,0 +1,9 @@
+//repaso c++
+#include <iostream>
+int main (void)
+{
+	std::cout <<"hola mundo!, standar output"<< std::endl;
+	std::cerr <<"hola mundo!, standar error"<< std::endl;
+
+return 0;
+}
