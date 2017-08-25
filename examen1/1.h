55e017f005441e7ab74f20fe33b2cdf2162d2762
ceamartinezri <ceamartinezri@unal.edu.co>
2017-08-25 11:39:20 -0500

    borro carmeta examen1

--- examen1/1.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <stdlib.h>
-#include<cstdlib>
-#include <cmath>
-
-double dump_heron_fun(double a,double b, double c);
-double smart_heron_fun(double a,double b, double c);
--- examen1/readme.txt
+++
@@ -1,5 +0,0 @@
-Examen 1
-
-Cesar Augusto Martinez Riaño
-Codigo 133606
-
