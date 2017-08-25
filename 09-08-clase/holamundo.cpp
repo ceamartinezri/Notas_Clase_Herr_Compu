fc916c0fe22e436cbae02f939f55771841656a35
ceamartinezri <ceamartinezri@unal.edu.co>
2017-08-25 11:41:23 -0500

    borro carpeta 0-08-clase

--- 09-08-clase/holamundo.cpp
+++
@@ -1,9 +0,0 @@
-//repaso c++
-#include <iostream>
-int main (void)
-{
-	std::cout <<"hola mundo!, standar output"<< std::endl;
-	std::cerr <<"hola mundo!, standar error"<< std::endl;
-
-return 0;
-}
--- 09-08-clase/readme.txt
+++
@@ -1,3 +0,0 @@
-REPOSITORIO PARA CLASES DE HERRAMIENTAS COMMPUTACIONALES
-
-https://github.com/ceamartinezri/Notas_Clase_Herr_Compu.git
