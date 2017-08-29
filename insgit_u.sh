637aee8a654bced6a1c8c12154694b6239030ffe
ceamartinezri <ceamartinezri@unal.edu.co>
2017-08-29 09:26:41 -0500

    update

--- insgit_u.sh
+++ insgit_u.sh
@@ -6,10 +6,10 @@
 #echo "Contraseña SIA:";
 #read password;
 
-$echo "https://$user:$password@proxyapp.unal.edu.co:8080/"
-echo "https://ceamartinezri:carolina0809@proxyapp.unal.edu.co:8080/"
+#echo "https://$user:$password@proxyapp.unal.edu.co:8080/"
+export "https://ceamartinezri:carolina0809@proxyapp.unal.edu.co:8080/"
 
-export https_proxy="https://$user:$password@proxyapp.unal.edu.co:8080/"
+#export https_proxy="https://$user:$password@proxyapp.unal.edu.co:8080/"
 
 echo "Escribe el mensaje del commit a realizar:";
 read message;
